433c6547a81ec3807e48b636c9dfbce127327c0f
Sachin Panayil <sachinpanayil01@gmail.com>
2026-02-02 17:18:52 -0500

    updating user names

--- flyway/sql/django_reference_data/R__2_create_npd_readonly_user.sql
+++ flyway/sql/django_reference_data/R__2_create_npd_readonly_user.sql
@@ -1,11 +1,11 @@
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'npd_readonly_db_user') THEN
-        CREATE USER npd_readonly WITH PASSWORD '${npdReadonlyUserPassword}';
+        CREATE USER npd_readonly_db_user WITH PASSWORD '${npdReadonlyUserPassword}';
     ELSE
-        ALTER USER npd_readonly WITH PASSWORD '${npdReadonlyUserPassword}';
+        ALTER USER npd_readonly_db_user WITH PASSWORD '${npdReadonlyUserPassword}';
     END IF;
 END
 $$;
 
-GRANT pg_read_all_data TO npd_readonly;
+GRANT pg_read_all_data TO npd_readonly_db_user;
