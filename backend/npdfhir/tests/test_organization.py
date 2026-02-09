33f4cb6b3e6b6873943137b210a6b311db43dec2
Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
2026-02-09 09:54:19 -0600

    NDH-811 revisions
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

--- backend/npdfhir/tests/test_organization.py
+++ backend/npdfhir/tests/test_organization.py
@@ -256,10 +256,31 @@ class OrganizationViewSetTestCase(APITestCase):
         assert_has_results(self, response)
 
     def test_list_filter_by_address_zipcode_leading_zero(self):
+        address_search = "404 Great Amazing Avenue Sandiego CA 05555"
         url = reverse("fhir-organization-list")
-        response = self.client.get(url, {"address": "404 Great Amazing Avenue Sandiego CA 05555"})
+        response = self.client.get(url, {"address": address_search})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         assert_has_results(self, response)
+
+        bundle = response.data["results"]
+
+        for entry in bundle["entry"]:
+            self.assertIn("resource", entry)
+            location_entry = entry["resource"]
+            self.assertIn("id", location_entry)
+            self.assertIn("contact", location_entry)
+            # self.assertIn("name", location_entry)
+
+            search_in_location_list = []
+
+            for address in location_entry["contact"]:
+                self.assertIn("address", address)
+                address_string = concat_address_string(address["address"])
+                search_in_location_list.append(address_search in address_string)
+
+            self.assertTrue(any(search_in_location_list))
+
+            self.assertEqual(location_entry["resourceType"], "Organization")
 
     def test_list_filter_by_address_city(self):
         url = reverse("fhir-organization-list")
@@ -280,10 +301,30 @@ class OrganizationViewSetTestCase(APITestCase):
         assert_has_results(self, response)
 
     def test_list_filter_by_address_zipcode_filter_leading_zero(self):
+        address_search = "05555"
         url = reverse("fhir-organization-list")
-        response = self.client.get(url, {"address_postalcode": "05555"})
+        response = self.client.get(url, {"address_postalcode": address_search})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         assert_has_results(self, response)
+
+        bundle = response.data["results"]
+
+        for entry in bundle["entry"]:
+            self.assertIn("resource", entry)
+            location_entry = entry["resource"]
+            self.assertIn("id", location_entry)
+            self.assertIn("contact", location_entry)
+            # self.assertIn("name", location_entry)
+
+            search_in_location_list = []
+
+            for address in location_entry["contact"]:
+                self.assertIn("address", address)
+                search_in_location_list.append(address_search in address["address"]["postalCode"])
+
+            self.assertTrue(any(search_in_location_list))
+
+            self.assertEqual(location_entry["resourceType"], "Organization")
 
     def test_list_filter_by_address_use(self):
         url = reverse("fhir-organization-list")
--- backend/npdfhir/tests/test_practitioner_role.py
+++ backend/npdfhir/tests/test_practitioner_role.py
@@ -564,6 +564,28 @@ class PractitionerRoleViewSetTestCase(APITestCase):
             location_obj = Location.objects.get(pk=location_id)
 
             self.assertEqual(zip_search, location_obj.address.address_us.zipcode)
+    
+    def test_list_filter_by_address_general_zip_leading_zero(self):
+        zip_search = "404 Great Amazing Avenue San Diego CA 05555"
+        url = reverse("fhir-practitionerrole-list")
+        response = self.client.get(url, {"location_address": zip_search})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert_has_results(self, response)
+
+        bundle = response.data["results"]
+
+        for entry in bundle["entry"]:
+            location_id = entry["resource"]["location"][0]["reference"].split("/")[-1]
+            self.assertIn("resource", entry)
+            location_entry = entry["resource"]
+
+            self.assertEqual(location_entry["resourceType"], "PractitionerRole")
+            self.assertIn("id", location_entry)
+            self.assertIn("active", location_entry)
+
+            location_obj = Location.objects.get(pk=location_id)
+
+            self.assertEqual(zip_search, location_obj.address.address_us.zipcode)
 
     def test_list_filter_by_endpoint_connection_type(self):
         connection_type_id = self.endpoint.endpoint_instance.endpoint_connection_type.id
