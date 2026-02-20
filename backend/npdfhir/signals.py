fc6e071198657624a9438e57dbba567d5ed3748f
Sachin Panayil <sachinpanayil01@gmail.com>
2026-02-20 15:12:09 -0500

    added userid logging

--- backend/npdfhir/signals.py
+++ backend/npdfhir/signals.py
@@ -2,6 +2,7 @@ import structlog
 from django.dispatch import receiver
 from django_structlog import signals
 from django_structlog.middlewares.request import get_request_header
+import hashlib
 
 
 @receiver(signals.bind_extra_request_metadata)
@@ -9,3 +10,12 @@ def bind_trace_id(request, logger, **kwargs):
     trace_id = get_request_header(request, "x-amzn-trace-id", "HTTP_X_AMZN_TRACE_ID")
     if trace_id:
         structlog.contextvars.bind_contextvars(trace_id=trace_id)
+
+
+@receiver(signals.bind_extra_request_metadata)
+def bind_user_id(request, logger, **kwargs):
+    if hasattr(request, "user") and request.user.is_authenticated:
+        # using a hashed id to make it opaque
+        user_id = hashlib.sha256(str(request.user.id).encode()).hexdigest()
+
+        structlog.contextvars.bind_contextvars(user_id=user_id, username=request.user.username)
