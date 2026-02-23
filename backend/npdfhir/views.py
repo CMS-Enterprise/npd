d001caed83bd4a6595c01f312d10aaa79a87b5c8
Isaac Milarsky <imilarsky@gmail.com>
2026-02-23 08:46:04 -0600

    NDH-801 reimplement sorting logic (#403)
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    Co-authored-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

--- backend/npdfhir/views.py
+++ backend/npdfhir/views.py
@@ -464,18 +464,11 @@ class FHIRCapabilityStatementView(APIView):
 
 
 class FHIROrganizationAffiliationViewSet(viewsets.GenericViewSet):
-    queryset = Organization.objects.none()
-    if DEBUG:
-        renderer_classes = [FHIRRenderer, BrowsableAPIRenderer]
-    else:
-        renderer_classes = [FHIRRenderer]
-    filter_backends = [DjangoFilterBackend, ParamOrderingFilter]
-    # filterset_class = OrganizationFilterSet
-    pagination_class = CustomPaginator
+    """
+    ViewSet for FHIR EHR Vendor to Organizaton relationships
+    """
 
-    ordering = ["organization_name"]
-    ordering_fields = ["ehr_vendor_name", "organization_name", "endpoint_name"]
-    lookup_url_kwarg = "id"
+    #queryset = Organization.objects.none()
 
     endpoint_subquery = LocationToEndpointInstance.objects.filter(
         location__organization=OuterRef("pk"), endpoint_instance__ehr_vendor__isnull=False
@@ -529,16 +522,35 @@ class FHIROrganizationAffiliationViewSet(viewsets.GenericViewSet):
             participating_npi=F("clinicalorganization__npi__npi"),
         )
         .distinct()
-        .order_by("organization_name")
     )
 
+    if DEBUG:
+        renderer_classes = [FHIRRenderer, BrowsableAPIRenderer]
+    else:
+        renderer_classes = [FHIRRenderer]
+    filter_backends = [DjangoFilterBackend, ParamOrderingFilter]
+    # filterset_class = OrganizationFilterSet
+    pagination_class = CustomPaginator
+
+    ordering = ["organization_name"]
+    ordering_fields = ["ehr_vendor_name", "organization_name"]
+    lookup_url_kwarg = "id"
+
+    # permission_classes = [permissions.IsAuthenticated]
     @extend_schema(
         responses={
             200: OpenApiResponse(description=docs.endpoints.organization_affiliation.list_response)
         }
     )
     def list(self, request):
-        paginated_organization_affiliations = self.paginate_queryset(self.queryset)
+        """
+        Query a list of organizations, represented as a bundle of FHIR Practitioner resources
+
+        Default sort order: ascending by organization name
+        """
+
+        organization_affiliation = self.filter_queryset(self.queryset)
+        paginated_organization_affiliations = self.paginate_queryset(organization_affiliation)
 
         serialized_organization_affiliations = OrganizationAffiliationSerializer(
             paginated_organization_affiliations, many=True, context={"request": request}
