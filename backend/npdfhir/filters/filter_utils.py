d7e0fb8bb1156461856fa0d1185a53fe1b62cb94
Isaac Milarsky <imilarsky@gmail.com>
2026-03-19 07:29:42 -0500

    [NDH-846] Update Practitioner Role Filters to Not Duplicate Logic (#422)
    * NDH-846 refactor filter identifier filters as well as address and field based searches
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * NDH-846 create generic gender filter method and use field based vector search method when applicable
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * NDH-846 formatting with ruff
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * try to fix end to end tests
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * playwright tests fail in github actions
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * NDH-846 refactor name search into generic method
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * hardcode final path for name and gender searches
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * NDH-846 hardcode end of path for address filters
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * NDH-846 use prefix logic for identifier logic
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * NDH-846 ruff formatting
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * NDH-846 organization name filter standardization
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * NDH-846 general nucc filter methods
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    * formatting
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    
    ---------
    
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
    Co-authored-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

---
+++ backend/npdfhir/filters/filter_utils.py
@@ -0,0 +1,158 @@
+import re
+from django.contrib.postgres.search import SearchVector, SearchQuery
+from django.db.models import Q
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import D
+
+from ..utils import parse_identifier_query
+from ..mappings import genderMapping, addressUseMapping
+
+
+def broad_address_match(queryset, name, value, prefix=""):
+    location_filter_paths = [
+        prefix + "address__address_us__delivery_line_1",
+        prefix + "address__address_us__delivery_line_2",
+        prefix + "address__address_us__city_name",
+        prefix + "address__address_us__state_code__abbreviation",
+        prefix + "address__address_us__zipcode",
+    ]
+
+    return queryset.annotate(search=SearchVector(*location_filter_paths)).filter(
+        search=SearchQuery(value, search_type="websearch", config="english")
+    )
+
+
+def field_based_vector_search(queryset, name, value, address_path):
+    return queryset.annotate(search=SearchVector(address_path)).filter(
+        search=SearchQuery(value, search_type="websearch", config="english")
+    )
+
+
+def city_address_search(queryset, name, value, prefix=""):
+    path = prefix + "address__address_us__city_name"
+    return field_based_vector_search(queryset, name, value, path)
+
+
+def state_address_search(queryset, name, value, prefix=""):
+    path = prefix + "address__address_us__state_code__abbreviation"
+    return field_based_vector_search(queryset, name, value, path)
+
+
+def postalcode_address_search(queryset, name, value, prefix=""):
+    path = prefix + "address__address_us__zipcode"
+    arg = {path: value}
+    return queryset.filter(**arg)
+
+
+def address_use_search(queryset, name, value, prefix=""):
+    if value in addressUseMapping.keys():
+        value = addressUseMapping.toNPD(value)
+    else:
+        value = -1
+
+    arg = {prefix + "__address_use_id": value}
+    return queryset.filter(**arg)
+
+
+def general_filter_distance(queryset, name, value, prefix=""):
+    pattern = r"(-?\d+\.?\d*)\|(-?\d+\.?\d*)\|(\d+\.?\d*)\|?(km|mi|ft)?"
+    match = re.fullmatch(pattern, value)
+    if match:
+        lat, lon, distance, units = match.groups()
+        lon = float(lon)
+        lat = float(lat)
+        distance = float(distance)
+        user_location = Point(lon, lat, srid=4326)
+        match units:
+            case "mi":
+                distance_function = D(mi=distance)
+            case "ft":
+                distance_function = D(ft=distance)
+            case _:
+                distance_function = D(km=distance)
+
+        arg = {
+            prefix + "address__address_us__geolocation__distance_lte": (
+                user_location,
+                distance_function,
+            )
+        }
+        return queryset.filter(**arg)
+    else:
+        return queryset.objects.none()
+
+
+def filter_identifier_general(
+    queryset, name, value, npi_prefix=None, ein_prefix=None, other_prefix=None
+):
+    from uuid import UUID
+
+    system, identifier_id = parse_identifier_query(value)
+    queries = Q(pk__isnull=True)
+
+    try:
+        npi_q_argument = {npi_prefix + "npi": int(identifier_id)}
+    except (ValueError, TypeError):
+        # TODO: implement validationerror to show users that NPI must be an int
+        npi_q_argument = None
+
+    if system:  # specific identifier search requested
+        if system.upper() == "NPI":
+            if npi_q_argument:
+                queries = Q(**npi_q_argument)
+    else:  # general identifier search requested
+        if npi_q_argument:
+            queries |= Q(**npi_q_argument)
+
+        if ein_prefix:
+            try:
+                ein_q_argument = {ein_prefix + "ein_id": identifier_id}
+                UUID(identifier_id)
+                queries |= Q(**ein_q_argument)
+            except (ValueError, TypeError):
+                pass
+
+        if other_prefix:
+            other_q_argument = {other_prefix + "other_id": identifier_id}
+            queries |= Q(**other_q_argument)
+
+    return queryset.filter(queries).distinct()
+
+
+def generic_filter_gender(queryset, name, value, prefix):
+    gender_path = prefix + "__individual__gender"
+    if value in genderMapping.keys():
+        param_map = {gender_path: genderMapping.toNPD(value)}
+        return queryset.filter(**param_map)
+    return queryset
+
+
+def simple_generic_field_search(queryset, name, value, name_path):
+    query = SearchQuery(value, search_type="websearch", config="english")
+
+    name_path_dict = {name_path: query}
+
+    return queryset.filter(**name_path_dict)
+
+
+def filter_individual_name(queryset, name, value, prefix):
+    path = prefix + "__individual__individualtoname__search_vector"
+    return simple_generic_field_search(queryset, name, value, path)
+
+
+def filter_organization_name_gen(queryset, name, value, prefix=""):
+    path = prefix + "organizationtoname__search_vector"
+    return simple_generic_field_search(queryset, name, value, path)
+
+
+def gen_nucc_code_filter(queryset, name, value, prefix=""):
+    path = prefix + "organization__clinicalorganization__organizationtotaxonomy__nucc_code__code"
+    return field_based_vector_search(queryset, name, value, path)
+
+
+def gen_nucc_display_filter(queryset, name, value, prefix=""):
+    path = (
+        prefix
+        + "organization__clinicalorganization__organizationtotaxonomy__nucc_code__display_name"
+    )
+    return field_based_vector_search(queryset, name, value, path)
--- backend/npdfhir/filters/organization_affiliation_filter_set.py
+++ backend/npdfhir/filters/organization_affiliation_filter_set.py
@@ -1,20 +1,27 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery
-from django.db.models import Q
 from django_filters import rest_framework as filters
 
-from ..utils import parse_identifier_query
 from ..models import Nucc, Location
+from .filter_utils import (
+    filter_identifier_general,
+    broad_address_match,
+    field_based_vector_search,
+    city_address_search,
+    state_address_search,
+    postalcode_address_search,
+)
 
 
 class OrganizationAffiliationFilterSet(filters.FilterSet):
-    primary_organization_name = filters.CharFilter(method="filter_name", help_text="Filter by organization name")
+    primary_organization_name = filters.CharFilter(
+        method="filter_name", help_text="Filter by organization name"
+    )
     participating_organization_name = filters.CharFilter(
         method="filter_participating_name", help_text="Filter by pariticipating organization name"
     )
 
     participating_organization_identifier = filters.CharFilter(
         method="filter_identifier",
-        help_text="Filter by identifier (NPI, EIN, or other). Format: value or system|value"
+        help_text="Filter by identifier (NPI, EIN, or other). Format: value or system|value",
     )
 
     participating_organization_type = filters.CharFilter(
@@ -25,7 +32,9 @@ class OrganizationAffiliationFilterSet(filters.FilterSet):
         method="filter_location", help_text="Filter by any part of address"
     )
 
-    location_address_city = filters.CharFilter(method="filter_address_city", help_text="Filter by city name")
+    location_address_city = filters.CharFilter(
+        method="filter_address_city", help_text="Filter by city name"
+    )
 
     location_address_state = filters.CharFilter(
         method="filter_address_state", help_text="Filter by state (2-letter abbreviation)"
@@ -36,31 +45,14 @@ class OrganizationAffiliationFilterSet(filters.FilterSet):
     )
 
     def filter_name(self, queryset, name, value):
-        query = SearchQuery(f"{value}", search_type="phrase")
-        return queryset.annotate(ehr_vendor_search=SearchVector("ehr_vendor_name")).filter(
-            ehr_vendor_search=query
-        )
+        return field_based_vector_search(queryset, name, value, "ehr_vendor_name")
 
     def filter_participating_name(self, queryset, name, value):
         return queryset.filter(organization_name__icontains=value)
 
+    # Filter generally by clinical organizational
     def filter_identifier(self, queryset, name, value):
-        system, identifier_id = parse_identifier_query(value)
-        queries = Q(pk__isnull=True)
-
-        if system:
-            if system.upper() == "NPI":
-                try:
-                    queries = Q(npi=int(identifier_id))
-                except (ValueError, TypeError):
-                    pass
-        else:
-            try:
-                queries |= Q(npi=int(identifier_id))
-            except (ValueError, TypeError):
-                pass
-
-        return queryset.filter(queries).distinct()
+        return filter_identifier_general(queryset, name, value, npi_prefix="")
 
     def filter_organization_type(self, queryset, name, value):
         # Get codes corresponding to the display name
@@ -71,47 +63,30 @@ class OrganizationAffiliationFilterSet(filters.FilterSet):
         return queryset.filter(taxonomy_codes__overlap=list(codes))
 
     def filter_location(self, queryset, name, value):
-        matching_location_ids = (
-            Location.objects.annotate(
-                location_search=SearchVector(
-                    "name",
-                    "address__address_us__delivery_line_1",
-                    "address__address_us__delivery_line_2",
-                    "address__address_us__city_name",
-                    "address__address_us__state_code__abbreviation",
-                    "address__address_us__zipcode",
-                )
-            )
-            .filter(location_search=SearchQuery(value, search_type="websearch", config="english"))
-            .values_list("id", flat=True)
+        matching_location_ids = broad_address_match(Location.objects, name, value).values_list(
+            "id", flat=True
         )
 
         # Filter affiliation rows where any location_id overlaps
         return queryset.filter(location_ids__overlap=list(matching_location_ids))
 
     def filter_address_city(self, queryset, name, value):
-        matching_location_ids = (
-            Location.objects.annotate(search=SearchVector("address__address_us__city_name"))
-            .filter(search=value)
-            .values_list("id", flat=True)
+        matching_location_ids = city_address_search(Location.objects, name, value).values_list(
+            "id", flat=True
         )
 
         return queryset.filter(location_ids__overlap=list(matching_location_ids))
 
     def filter_address_state(self, queryset, name, value):
-        matching_location_ids = (
-            Location.objects.annotate(
-                search=SearchVector("address__address_us__state_code__abbreviation")
-            )
-            .filter(search=value)
-            .values_list("id", flat=True)
+        matching_location_ids = state_address_search(Location.objects, name, value).values_list(
+            "id", flat=True
         )
 
         return queryset.filter(location_ids__overlap=list(matching_location_ids))
 
     def filter_address_postalcode(self, queryset, name, value):
-        matching_location_ids = Location.objects.filter(
-            address__address_us__zipcode=value
+        matching_location_ids = postalcode_address_search(
+            Location.objects, name, value
         ).values_list("id", flat=True)
 
         return queryset.filter(location_ids__overlap=list(matching_location_ids))
