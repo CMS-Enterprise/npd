9070f72cafed49733a3a83a86206d16c5e685448
Blaine Price <william.price@cms.hhs.gov>
2026-04-01 13:38:23 -0400

    configure cache middleware

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -77,6 +77,7 @@ if DEBUG and not TESTING:
     INSTALLED_APPS.append("debug_toolbar")
 
 MIDDLEWARE = [
+    "django.middleware.cache.UpdateCacheMiddleware",
     "django_structlog.middlewares.RequestMiddleware",
     "npdfhir.middleware.HealthCheckMiddleware",
     "corsheaders.middleware.CorsMiddleware",
@@ -89,6 +90,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.cache.FetchFromCacheMiddleware"
 ]
 
 if REQUIRE_AUTHENTICATION:
